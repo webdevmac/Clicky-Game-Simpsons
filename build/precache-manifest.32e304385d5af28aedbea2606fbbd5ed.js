ca9ea88a10a6b4da561bf98c40f4fa31c03f1ec7
Web-dev <webdevmac@users.noreply.github.com>
2019-03-23 09:41:29 -0400

    update

---
+++ build/precache-manifest.32e304385d5af28aedbea2606fbbd5ed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "379f6d87fb26baf96301",
+    "url": "/Clicky-Game-Simpsons/static/js/runtime~main.b9b2785c.js"
+  },
+  {
+    "revision": "f6cfd53a12370597e5cf",
+    "url": "/Clicky-Game-Simpsons/static/js/main.51a86075.chunk.js"
+  },
+  {
+    "revision": "f05a1df3217379e807f2",
+    "url": "/Clicky-Game-Simpsons/static/js/2.fba19b92.chunk.js"
+  },
+  {
+    "revision": "f6cfd53a12370597e5cf",
+    "url": "/Clicky-Game-Simpsons/static/css/main.bd84a047.chunk.css"
+  },
+  {
+    "revision": "554296cf08b036d977e19a43f74e5c83",
+    "url": "/Clicky-Game-Simpsons/index.html"
+  }
+];
